350b6c01489999d5495821bb694005597f7e34b9
Jordan Faust <jfaust47@gmail.com>
2022-07-16 14:51:22 -0500

    Fix Doom Emacs installation
    * Clone Doom Emacs if it is missing
    * Clone personal doom configuration and keep it out of nix
    * Extract doom configuration from repo

--- modules/editors/emacs.nix
+++ modules/editors/emacs.nix
@@ -10,6 +10,7 @@ in {
     doom = rec {
       enable = mkBoolOpt false;
       repoUrl = mkOpt types.str "https://github.com/doomemacs/doomemacs";
+      configRepoUrl = mkOpt types.str "https://github.com/JordanFaust/doom-config";
     };
   };
 
@@ -41,15 +42,23 @@ in {
       (aspellWithDicts (ds: with ds; [
         en en-computers en-science
       ]))
+
       # :tools editorconfig
       editorconfig-core-c # per-project style config
+      # :tools terraform
+      terraform
       # :tools lookup & :lang org +roam
       sqlite
+
       # :lang latex & :lang org (latex previews)
       texlive.combined.scheme-medium
       # :lang beancount
       beancount
       unstable.fava  # HACK Momentarily broken on nixos-unstable
+      # :lang markdown
+      # TODO
+      # :lang org
+      maim
     ];
 
     env.PATH = [ "$XDG_CONFIG_HOME/emacs/bin" ];
@@ -58,15 +67,25 @@ in {
 
     fonts.fonts = [ pkgs.emacs-all-the-icons-fonts ];
 
-    home.configFile = {
-      # TODO move doom into the config directory
-      "doom" = { source = "${configDir}/doom"; recursive = true; };
-    };
+    # Leaving this for historical reference. Allowing Nix to own my emacs
+    # configuration allows it to be rolled into the rebuild process. It does this
+    # at the cost of easy iteration of my configuration. Until my emacs configuration
+    # stabilizes this will live outside of nix within it's own repo
+    # home.configFile = {
+    #   # "doom" = { source = "${configDir}/doom"; recursive = true; };
+    # };
 
+    # Clones Doom Emacs and my personal configuration. This does not include the
+    # steps to install doom and sync/build the packages from my personal config
     system.userActivationScripts = mkIf cfg.doom.enable {
       installDoomEmacs = ''
-        if [ ! -d "$XDG_CONFIG_HOME/emacs" ]; then
-           git clone --depth=1 --single-branch "${cfg.doom.repoUrl}" "$XDG_CONFIG_HOME/emacs"
+        #!${pkgs.stdenv.shell}
+        echo "Checking if Doom Emacs is installed"
+        if [ ! -f "$XDG_CONFIG_HOME/emacs/bin/doom" ]; then
+          echo "Cloning Doom Emacs"
+          ${pkgs.git}/bin/git clone --depth=1 --single-branch "${cfg.doom.repoUrl}" "$XDG_CONFIG_HOME/emacs"
+          echo "Cloning Doom Config"
+          ${pkgs.git}/bin/git clone "${cfg.doom.configRepoUrl}" "$XDG_CONFIG_HOME/doom"
         fi
       '';
     };
