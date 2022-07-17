a3378a92893970a71363f90657464a7b335199a2
Jordan Faust <jfaust47@gmail.com>
2022-07-16 22:08:52 -0500

    WIP Create my vilebloom theme

---
+++ modules/themes/vilebloom/default.nix
@@ -0,0 +1,138 @@
+# modules/themes/vilebloom/default.nix --- a pokemon and keyboard inspired theme
+
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.theme;
+in {
+  config = mkIf (cfg.active == "vilebloom") (mkMerge [
+    # Desktop-agnostic configuration
+    {
+      modules = {
+        theme = {
+          wallpaper = mkDefault ./config/wallpaper.jpg;
+          gtk = {
+            theme = "Dracula";
+            iconTheme = "Paper";
+            cursorTheme = "Paper";
+          };
+          fonts = {
+            sans.name = "Fira Sans";
+            mono.name = "Fira Code";
+          };
+          colors = {
+            black         = "#24455b";
+            red           = "#ff7771";
+            green         = "#d97a9b";
+            yellow        = "#ffba95";
+            blue          = "#4684ae";
+            magenta       = "#325f7d";
+            cyan          = "#c7bcba";
+            silver        = "#dfd9d8";
+            grey          = "#24455b";
+            brightred     = "#ff777a";
+            brightgreen   = "#d97a9b";
+            brightyellow  = "#ffba95";
+            brightblue    = "#4684ae";
+            brightmagenta = "#325f7d";
+            brightcyan    = "#c7bcba";
+            white         = "#ded8d7";
+
+            types.fg      = "#ded8d7";
+            types.panelbg = "#152733";
+            types.border  = "#ffba95";
+          };
+        };
+
+        shell.zsh.rcFiles  = [ ./config/zsh/prompt.zsh ];
+        shell.tmux.rcFiles = [ ./config/tmux.conf ];
+        desktop.browsers = {
+          firefox.userChrome = concatMapStringsSep "\n" readFile [
+            ./config/firefox/userChrome.css
+          ];
+        };
+      };
+    }
+
+    # Desktop (X11) theming
+    (mkIf config.services.xserver.enable {
+      user.packages = with pkgs; [
+        unstable.dracula-theme
+        paper-icon-theme # for rofi
+      ];
+      fonts = {
+        fonts = with pkgs; [
+          fira-code
+          fira-code-symbols
+          open-sans
+          jetbrains-mono
+          siji
+          font-awesome
+        ];
+      };
+
+      # Compositor
+      services.picom = {
+        fade = true;
+        fadeDelta = 1;
+        fadeSteps = [ 0.01 0.012 ];
+        shadow = true;
+        shadowOffsets = [ (-10) (-10) ];
+        shadowOpacity = 0.22;
+        # activeOpacity = "1.00";
+        # inactiveOpacity = "0.92";
+        settings = {
+          shadow-radius = 12;
+          # blur-background = true;
+          # blur-background-frame = true;
+          # blur-background-fixed = true;
+          blur-kern = "7x7box";
+          blur-strength = 320;
+        };
+      };
+
+      # Login screen theme
+      services.xserver.displayManager.lightdm.greeters.mini.extraConfig = ''
+        text-color = "${cfg.colors.magenta}"
+        password-background-color = "${cfg.colors.black}"
+        window-color = "${cfg.colors.types.border}"
+        border-color = "${cfg.colors.types.border}"
+      '';
+
+      # Other dotfiles
+      home.configFile = with config.modules; mkMerge [
+        {
+          # Sourced from sessionCommands in modules/themes/default.nix
+          "xtheme/90-theme".source = ./config/Xresources;
+        }
+        (mkIf desktop.bspwm.enable {
+          "bspwm/rc.d/00-theme".source = ./config/bspwmrc;
+          "bspwm/rc.d/95-polybar".source = ./config/polybar/run.sh;
+        })
+        (mkIf desktop.apps.rofi.enable {
+          "rofi/theme" = { source = ./config/rofi; recursive = true; };
+        })
+        (mkIf (desktop.bspwm.enable || desktop.stumpwm.enable) {
+          "polybar" = { source = ./config/polybar; recursive = true; };
+          "dunst/dunstrc".text = import ./config/dunstrc cfg;
+          "Dracula-purple-solid-kvantum" = {
+            recursive = true;
+            source = "${pkgs.unstable.dracula-theme}/share/themes/Dracula/kde/kvantum/Dracula-purple-solid";
+            target = "Kvantum/Dracula-purple-solid";
+          };
+          "kvantum.kvconfig" = {
+            text = "theme=Dracula-purple-solid";
+            target = "Kvantum/kvantum.kvconfig";
+          };
+        })
+        (mkIf desktop.media.graphics.vector.enable {
+          "inkscape/templates/default.svg".source = ./config/inkscape/default-template.svg;
+        })
+        (mkIf desktop.browsers.qutebrowser.enable {
+          "qutebrowser/extra/theme.py".source = ./config/qutebrowser/theme.py;
+        })
+      ];
+    })
+  ]);
+}
