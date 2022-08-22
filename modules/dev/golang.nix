1dce08e9850f2cd100b2ae9e4b28d5a324007e8b
Jordan Faust <jfaust47@gmail.com>
2022-08-22 10:40:02 -0500

    feat: Add support for go development

---
+++ modules/dev/golang.nix
@@ -0,0 +1,24 @@
+# modules/dev/golang.nix
+
+{ config, options, lib, pkgs, my, ... }:
+
+with lib;
+with lib.my;
+let devCfg = config.modules.dev;
+    cfg = devCfg.golang;
+in {
+  options.modules.dev.golang = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkMerge [
+    (mkIf cfg.enable {
+      user.packages = with pkgs; [
+        # The specific language version
+        go
+        # Global gems
+        gopls
+      ];
+    })
+  ];
+}
