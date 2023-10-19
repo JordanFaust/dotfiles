24fe621ce85a7cdb4491ad0bf7fd4d9579f405a8
Jordan Faust <jfaust47@gmail.com>
2023-10-19 10:55:37 -0500

    feat: update to latest linux and improve hey

--- default.nix
+++ default.nix
@@ -54,7 +54,7 @@ with lib.my;
 
   # Use the latest kernel
   boot = {
-    kernelPackages = mkDefault pkgs.linuxKernel.packages.linux_5_18;
+    kernelPackages = mkDefault pkgs.linuxKernel.packages.linux_6_5;
     loader = {
       efi.canTouchEfiVariables = mkDefault true;
       systemd-boot.configurationLimit = 10;
