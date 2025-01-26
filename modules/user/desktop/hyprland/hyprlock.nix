7ef73011afe86babbe9273e1216e59ca5144c54c
Jordan Faust <jordan.faust@procore.com>
2025-01-25 21:38:28 -0600

    feat(hyprlock): redesign hyprlock look and feel

--- modules/user/desktop/hyprland/hyprlock.nix
+++ modules/user/desktop/hyprland/hyprlock.nix
@@ -14,7 +14,10 @@ in {
       settings = {
         # General Configuraiton
         general = {
-          grace = 5;
+          grace = 1;
+          ignore_empty_input = true;
+          text_trim = false;
+          disable_loading_bar = true;
         };
 
         # Background Configuration
@@ -33,6 +36,7 @@ in {
             brightness = 0.8000;
             vibrancy = 0.2100;
             vibrancy_darkness = 0.0;
+            zindex = -1;
           }
         ];
 
@@ -40,57 +44,134 @@ in {
         input-field = [
           {
             # monitor = "";
-            size = "250, 50";
-            outline_thickness = 3;
+            size = "350, 70";
+            outline_thickness = 4;
             dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8
             dots_spacing = 0.64; # Scale of dots' absolute size, 0.0 - 1.0
             dots_center = true;
             outer_color = "rgb(ED8796)";
             inner_color = "rgb(24273A)";
             font_color = "rgb(ded8d7)";
-            fade_on_empty = true;
-            placeholder_text = "<i>Password...</i>";
+            fade_on_empty = false;
+            font_family = "MonoLisa Variable Regular";
+            placeholder_text = "<i><span foreground='##ded8d7'>Input Password...</span></i>";
             hide_input = false;
-            position = "0, 50";
+            position = "0, -600";
+            halign = "center";
+            valign = "center";
+            zindex = 10;
+          }
+        ];
+
+        shape = [
+          # Big Rectangle
+          {
+            size = "100%, 80";
+            color = "rgb(24273A)";
             halign = "center";
             valign = "bottom";
+            zindex = 0;
+          }
+
+          # Small Rectangle for Session
+          {
+            size = "250, 52";
+            rounding = "10";
+            color = "rgb(ED8796)";
+            halign = "center";
+            valign = "bottom";
+            position = "0, 14";
+            zindex = 1;
+          }
+        ];
+
+        image = [
+          # PFP Image
+          {
+              path = "$HOME/.face";
+              size = "140";
+              rounding = "-1";
+              border_size = "3";
+              border_color = "rgba(ded8d7FF)";
+              position = "10, 10";
+              halign = "left";
+              valign = "bottom";
+              zindex = "3";
           }
         ];
 
         label = [
+
+         # Lock Icon
+          {
+              text = " ";
+              shadow_passes = "1";
+              shadow_boost = "0.5";
+              color = "rgba(ded8d7FF)";
+              shadow_color = "rgba(ded8d7AA)";
+              font_size = "20";
+              font_family = "Font Awesome 6 Free Solid";
+              position = "0, -140";
+              halign = "center";
+              valign = "top";
+              zindex = "2";
+          }
           # Current Time
           {
             # monitor = "";
-            text = "<b><big>$TIME</big></b>";
-            text_align = "center";
+            text = "$TIME";
             color = "rgb(ded8d7)";
-            font_size = 84;
-            font_family = "MonoLisa Variable Regular";
-            position = "0, 100";
+            font_size = 120;
+            shadow_passes = 3;
+            shadow_boost = 0.5;
+            font_family = "MonoLisa Variable Bold";
+            position = "0, -200";
             halign = "center";
-            valign = "center";
+            valign = "top";
+            zindex = 3;
           }
-          # User
+          # Current Session Status
           {
-            # monitor = "";
-            text = "Hey <span text_transform=\"capitalize\" size=\"larger\">$USER</span>";
-            color = "rgb(ded8d7)";
-            font_size = 20;
-            font_family = "MonoLisa Variable Regular";
-            position = "0, 0";
-            halign = "center";
-            valign = "center";
-          }
-          # Password Field
-          {
-            # monitor = "";
-            text = "Type to unlock!";
-            color = "rgb(ded8d7)";
+            text = "cmd[update:24000000] echo \"Session: \$XDG_SESSION_DESKTOP\"";
+            color = "rgba(24273AFF)";
+            shadow_color = "rgba(24273AAA)";
             font_size = 16;
-            font_family = "MonoLisa Variable Regular";
-            position = "0, 32";
+            font_family = "MonoLisa Variable Medium";
+            position = "0, -4";
             halign = "center";
             valign = "bottom";
+            zindex = 2;
+          }
+
+          # Username
+          {
+            # monitor = "";
+            # text = "Hey <span text_transform=\"capitalize\" size=\"larger\">$USER</span>";
+            text = "$USER";
+            shadow_passes = 1;
+            shadow_boost = 0.5;
+            color = "rgba(ED8796BB)";
+            shadow_color = "rgba(ED8796AA)";
+            font_size = 20;
+            font_family = "MonoLisa Variable Italic Medium";
+            position = "160, 40";
+            halign = "left";
+            valign = "bottom";
+            zindex = 2;
+          }
+          # Hostname
+          {
+            text = "cmd[update:24000000] echo \"@\$(uname -n)\"";
+            shadow_passes = 1;
+            shadow_boost = 0.5;
+            color = "rgba(ded8d7BB)";
+            shadow_color = "rgba(ded8d7AA)";
+            font_size = 18;
+            font_family = "MonoLisa Variable Italic";
+            position = "160, -22";
+            halign = "left";
+            valign = "bottom";
+            zindex = 2;
           }
         ];
       };
